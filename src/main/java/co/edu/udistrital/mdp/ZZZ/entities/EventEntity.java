3b4d5a8120c34ac9b0b58286ab6b41e8728b9f00
Anggel26 <anggelleal26@gmail.com>
2026-09-02 01:16:01 -0500

    feat: add Message, Notification, Event entities and notification subclasses

---
+++ src/main/java/co/edu/udistrital/mdp/ZZZ/entities/EventEntity.java
@@ -0,0 +1,33 @@
+package co.edu.udistrital.mdp.ZZZ.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+// Event organized by a shelter
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+public class EventEntity extends BaseEntity {
+
+	// Internal event identifier
+	private Integer eventId;
+
+	// Name of the event
+	private String name;
+
+	// Date the event takes place
+	@Temporal(TemporalType.DATE)
+	private Date date;
+
+	// Time the event starts
+	private String time;
+
+	// Where the event is held
+	private String location;
+}
